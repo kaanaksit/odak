9a665c39562ab271ee797c863396ff7d90301bac
Kaan Akşit <kunguz@gmail.com>
2019-12-15 13:14:24 -0800

    First working setup script.

---
+++ setup.py
@@ -0,0 +1,34 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+      name             = "Odak",
+      version          = "0.1",
+      author           = "Kaan Akşit",
+      author_email     = "kunguz@gmail.com",
+      description      = (
+                          "Odak (pronounced O-dawec) is the fundamental Python library"
+                          "for scientific computing in optical sciences."
+                          "Odak includes modules for geometric raytracing and wave optics."
+                         ),
+      license          = "",
+      keywords         = "example documentation tutorial",
+      url              = "https://github.com/kunguz/odak",
+      packages         = [
+                          'odak',
+                          'odak/raytracing',
+                          'odak/tools'
+                         ],
+      long_description = read('README.md'),
+      classifiers      = [
+                          "Development Status :: 2 - Pre-Alpha",
+                          "Intended Audience :: Science/Research",
+                          "Intended Audience :: Developers",
+                          "Topic :: Scientific/Engineering :: Physics",
+                          "Programming Language :: Python",
+                          "License :: OSI Approved :: Apache Software License",
+                         ],
+      )
