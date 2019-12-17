f95d937f59b3aba9c5ce6b842d72115d1e4ac25d
Kaan Akşit <kunguz@gmail.com>
2019-12-16 22:07:58 -0800

    Renaming the test script.

--- odak/test/test_import.py
+++ odak/test/test_import.py
@@ -1,22 +1,10 @@
 #!/usr/bin/env python
-#
-# This allows running the tests for odak:
-#
-#   $ python tests.py
-#
-# See help for more arguments.
-#
-#   $ pyton test.py --help
-#
 
 import sys
-import argparse
 
 def test_odak_import():
     import odak
     assert True=True
 
 if __name__ == '__main__':
-
-    
     sys.exit(test_odak_import())
