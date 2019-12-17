678de8ed4061803d19d281ae81d3d050df7d9ee5
Kaan Akşit <kunguz@gmail.com>
2019-12-16 21:57:34 -0800

    Adding a test script.

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
                           'odak',
                           'odak/raytracing',
                           'odak/tools',
+                          'odak/test',
                           'odak/oldschool'
                          ],
       long_description = read('README.md'),
---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#
+# This allows running the tests for odak:
+#
+#   $ python tests.py
+#
+# See help for more arguments.
+#
+#   $ pyton test.py --help
+#
+
+import sys
+import argparse
+
+if __name__ == '__main__':
+
+    import odak
+    sys.exit(1)
