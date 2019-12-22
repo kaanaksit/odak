04d3028c193df69b44829dfe3eaf0961a643edfb
Kaan <kunguz@gmail.com>
2019-12-21 21:31:54 -0800

    Extending sample submodule and adding a test regarding to it.

---
+++ odak/test/test_sample_ray.py
@@ -0,0 +1,25 @@
+import odak.raytracing as raytracer
+import sys
+from odak.tools.sample import grid_sample,batch_of_rays
+
+def test():
+    sample_entry_points = grid_sample(
+                                      no=[4,4],
+                                      size=[100.,100.],
+                                      center=[0.,0.,0.],
+                                      angles=[0.,0.,0.]
+                                     )
+    sample_exit_points  = grid_sample(
+                                      no=[4,4],
+                                      size=[100.,100.],
+                                      center=[0.,0.,100.],
+                                      angles=[0.,0.,0.]
+                                     )
+    rays                = batch_of_rays(
+                                        sample_entry_points,
+                                        sample_exit_points
+                                       )
+    assert True==True
+
+if __name__ == '__main__':
+    sys.exit(test())
--- odak/tools/sample.py
+++ odak/tools/sample.py
@@ -44,22 +44,42 @@ def grid_sample(no=[10,10],size=[100.,100.],center=[0.,0.,0.],angles=[0.,0.,0.])
             point[1]       += center[1]
             point[2]       += center[2]
             samples[i,j,:]  = point
+    samples = samples.reshape((samples.shape[0]*samples.shape[1],samples.shape[2]))
     return samples
 
 def batch_of_rays(entry,exit):
     """
-    Definition to generate a batch of rays with given entry point(s) and exit point(s). Note that the mapping is one to one, meaning nth item in your entry points list will exit from nth item in your exit list and generate that particular ray.
+    Definition to generate a batch of rays with given entry point(s) and exit point(s). Note that the mapping is one to one, meaning nth item in your entry points list will exit from nth item in your exit list and generate that particular ray. Note that you can have a combination like nx3 points for entry or exit and 1 point for entry or exit. But if you have multiple points both for entry and exit, the number of points have to be same both for entry and exit.
 
     Parameters
     ----------
     entry      : ndarray
                  Either a single point with size of 3 or multiple points with the size of nx3.
-    exit       : ndarray            
+    exit       : ndarray
+                 Either a single point with size of 3 or multiple points with the size of nx3.
 
     Returns
     ----------
     rays       : ndarray
                  Generated batch of rays.
     """
-    
+    norays = [0,0]
+    if len(entry.shape) == 1:
+        entry = entry.reshape((1,3))
+    if len(exit.shape) == 1:
+        exit = exit.reshape((1,3))
+    norays = np.amax([entry.shape[0],exit.shape[0]])
+    if norays > exit.shape[0]:
+        exit = np.repeat(exit,norays,axis=0)
+    elif norays > entry.shape[0]:
+        entry = np.repeat(entry,norays,axis=0)
+    rays   = []
+    for i in range(norays):
+        rays.append(
+                    create_ray_from_two_points(
+                                               entry[i],
+                                               exit[i]
+                                              )
+                   )
+    rays   = np.asarray(rays)
     return rays
