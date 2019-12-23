33ee9167fc40df178989460f8d7ad777488de989
Kaan <kunguz@gmail.com>
2019-12-22 21:24:17 -0800

    Fixing Travis CI by changing the path in test_PLY.

--- test/test_PLY.py
+++ test/test_PLY.py
@@ -1,10 +1,10 @@
 import odak.raytracing as raytracer
 import sys
+import numpy as np
 from odak.tools.asset import read_PLY
 
 def test():
-    mesh = read_PLY('sample.ply')
-    import numpy as np
+    mesh = read_PLY('./test/)sample.ply')
     mesh = np.array(mesh)
     mesh = mesh.reshape((100,100,3))
     print(mesh.shape)
