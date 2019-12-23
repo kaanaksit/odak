1be60b2c088897e70465dace76c79d7772e2558a
Kaan <kunguz@gmail.com>
2019-12-23 15:10:20 -0800

    Updating README.

--- test/test_PLY.py
+++ test/test_PLY.py
@@ -5,7 +5,6 @@ from odak.tools.asset import read_PLY,write_PLY
 
 def test():
     mesh = read_PLY('./test/sample.ply')
-    mesh = np.array(mesh)
     mesh = mesh.reshape((100,100,3))
     write_PLY(mesh,savefn='output.ply')
     assert True==True
