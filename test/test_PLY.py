f03300afba1f4e2dcac604c607d06691e2f9d006
Kaan <kunguz@gmail.com>
2019-12-23 09:31:30 -0800

    Removing the print form the test.

--- test/test_PLY.py
+++ test/test_PLY.py
@@ -7,7 +7,6 @@ def test():
     mesh = read_PLY('./test/sample.ply')
     mesh = np.array(mesh)
     mesh = mesh.reshape((100,100,3))
-    print(mesh.shape)
     write_PLY(mesh,savefn='output.ply')
     assert True==True
 
