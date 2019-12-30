d92ba77cf0454978456a2803430bfeb29b47cd04
Kaan <kunguz@gmail.com>
2019-12-30 15:18:56 -0800

    More towards parallelization. Crazy crazy crazy speed ups.

--- odak/raytracing/boundary.py
+++ odak/raytracing/boundary.py
@@ -10,27 +10,30 @@ def reflect(input_ray,normal):
     Parameters
     ----------
     input_ray    : ndarray
-                   A vector/ray.
+                   A vector/ray (2x3). It can also be a list of rays (nx2x3).
     normal       : ndarray
-                   A surface normal.
+                   A surface normal (2x3). It also be a list of normals (nx2x3).
 
     Returns
     ----------
     output_ray   : ndarray
                    Array that contains starting points and cosines of a reflected ray.
     """
-    mu               = 1
-    div              = pow(normal[1,0],2)  + pow(normal[1,1],2) + pow(normal[1,2],2)
-    a                = mu* ( input_ray[1][0]*normal[1][0]
-                           + input_ray[1][1]*normal[1][1]
-                           + input_ray[1][2]*normal[1][2]) / div 
-    output_ray       = input_ray.copy()
-    output_ray[0][0] = normal[0][0]
-    output_ray[0][1] = normal[0][1]
-    output_ray[0][2] = normal[0][2]
-    output_ray[1][0] = input_ray[1][0] - 2*a*normal[1][0]
-    output_ray[1][1] = input_ray[1][1] - 2*a*normal[1][1]
-    output_ray[1][2] = input_ray[1][2] - 2*a*normal[1][2]
+    if len(input_ray.shape) == 2:
+        input_ray = input_ray.reshape((1,2,3))
+    if len(normal.shape) == 2:
+        normal    = normal.reshape((1,2,3))
+    mu              = 1
+    div             = normal[:,1,0]**2  + normal[:,1,1]**2 + normal[:,1,2]**2
+    a               = mu* ( input_ray[:,1,0]*normal[:,1,0]
+                          + input_ray[:,1,1]*normal[:,1,1]
+                          + input_ray[:,1,2]*normal[:,1,2]) / div 
+    n               = np.amax([normal.shape[0],input_ray.shape[0]])
+    output_ray      = np.zeros((n,2,3))
+    output_ray[:,0] = normal[:,0]
+    output_ray[:,1] = input_ray[:,1]-2*normal[:,1]
+    if output_ray.shape[0] == 1:
+       output_ray = output_ray.reshape((2,3))
     return output_ray
 
 def intersect_w_surface(ray,points):
@@ -52,12 +55,23 @@ def intersect_w_surface(ray,points):
                    Distance in between starting point of a ray with it's intersection with a planar surface.
     """
     normal               = get_triangle_normal(points)
-    f                    = points[0]-ray[0]
-    distance             = np.dot(normal[1],f)/np.dot(normal[1],ray[1])
-    normal[0][0]         = ray[0][0]+distance*ray[1][0]
-    normal[0][1]         = ray[0][1]+distance*ray[1][1]
-    normal[0][2]         = ray[0][2]+distance*ray[1][2]
+    if len(ray.shape) == 2:
+        ray = ray.reshape((1,2,3))
+    if len(points) == 2:
+        points = points.reshape((1,3,3))
+    if len(normal.shape) == 2:
+        normal = normal.reshape((1,2,3))
+    f                    = points[:,0]-ray[:,0]
+    distance             = np.dot(normal[:,1],f.T)/np.dot(normal[:,1],ray[:,1].T)
+    n                    = np.amax([ray.shape[0],normal.shape[0]])
+    normal               = np.zeros((n,2,3))
+    normal[:,0]          = ray[:,0]+distance[:,0]*ray[:,1]
     distance             = np.abs(distance)
+    if normal.shape[0] == 1:
+        normal   = normal.reshape((2,3))
+        distance = distance.reshape((1))
+    if distance.shape[0] == 1:
+        distance = distance.reshape((distance.shape[1]))
     return normal,distance
 
 def get_triangle_normal(triangle,triangle_center=None):
@@ -92,7 +106,7 @@ def get_triangle_normal(triangle,triangle_center=None):
 
 def intersect_w_circle(ray,circle):
     """
-    Definition to find intersection point of a ray with a circle. Returns False for each variable if the ray doesn't intersect with a given circle.
+    Definition to find intersection point of a ray with a circle. Returns False for each variable if the ray doesn't intersect with a given circle. Returns distance as zero if there isn't an intersection.
 
     Parameters
     ----------
@@ -109,9 +123,10 @@ def intersect_w_circle(ray,circle):
                    Distance in between a starting point of a ray and the intersection point with a given triangle.
     """
     normal,distance    = intersect_w_surface(ray,circle[0])
-    distance_to_center = distance_between_two_points(normal[0],circle[1])
-    if distance_to_center > circle[2]:
-        return False,False
+    if len(normal.shape) == 2:
+        normal = normal.reshape((1,2,3))
+    distance_to_center                     = distance_between_two_points(normal[:,0],circle[1])
+    distance[np.nonzero(distance_to_center>circle[2])] = 0
     return normal,distance
 
 def intersect_w_triangle(ray,triangle):
