6117242d017511088b12510d9555b8e0ce7c4993
Kaan Akşit <kunguz@gmail.com>
2020-01-20 20:42:45 -0800

    Blender GUI isn't frozen any more and server is working.

--- odak/visualize/blender/server.py
+++ odak/visualize/blender/server.py
@@ -1,28 +1,50 @@
-import bpy
+import bpy,queue
 import functools
-import socket
+import threading
+import os,socket,traceback
 
-def process(server,path_max):
-    connection, address = server.accept()
-    buf                 = connection.recv(path_max)
-    for cmd in buf.split(b'\x00'):
-        if cmd:
-            try:
-                cmd = cmd.decode()
-                print(cmd)
-                exec(cmd)
-            except:
-                import traceback
-                traceback.print_exc()
-                server.close()
-    return 1.0
+execution_queue = queue.Queue()
 
-port     = 8082
-host     = 'localhost'
-path_max = 4096
-server   = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind((host,port))
-server.listen(1)
-print("Listening on %s:%s" % (host, port))
+class whisper2blender(threading.Thread):
+    def __init__(self,port=8082,host='127.0.0.1',path_max=4096):
+        threading.Thread.__init__(self)
+        self.port     = port
+        self.host     = host
+        self.path_max = path_max
+    def run(self):
+        serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serversocket.bind((self.host, self.port))
+        serversocket.listen(1)
+        print("Listening on %s:%s" % (self.host, self.port))
+        while True:
+            connection, address = serversocket.accept()
+            buf                 = connection.recv(self.path_max)
+            for cmd in buf.split(b'\x00'):
+                if cmd:
+                    try:
+                        cmd = cmd.decode()
+#                        print(cmd)
+                        run_in_main_thread(cmd)
+    #                    exec(cmd)
+                    except:
+                        traceback.print_exc()
 
-bpy.app.timers.register(functools.partial(process,server,path_max), first_interval=1.0)
+def run_in_main_thread(function):
+    execution_queue.put(function)
+
+def execute_queued_functions():
+    while not execution_queue.empty():
+        function = execution_queue.get()
+        if type(function) != type(None):
+            exec(function)
+    return 0.1
+
+def main():
+    messenger = whisper2blender()
+    messenger.start()
+    bpy.app.timers.register(execute_queued_functions)
+    return True
+
+if __name__ == "__main__":
+    main()
+
