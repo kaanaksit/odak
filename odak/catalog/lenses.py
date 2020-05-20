bf42df08aa776a56c414a9ea118b02785bf57c61
Kaan Akşit <kunguz@gmail.com>
2020-05-19 22:14:45 -0700

    odak.catalog.planoe_convex_lens(): Plano-convex lens class is improved.

--- odak/catalog/lenses.py
+++ odak/catalog/lenses.py
@@ -5,7 +5,7 @@ import odak.tools as tools
 import odak.raytracing as raytracer
 
 class plano_convex_lens():
-    def __init__(self,item='LA1024'):
+    def __init__(self,item='LA1024',location=[0.,0.,0.],rotation=[0.,0.,0.]):
         """
         Class to represent plano-convex lens.
 
@@ -13,13 +13,51 @@ class plano_convex_lens():
         ----------
         item        : str
                       Plano convex lens label. Check Thorlabs catalog for labels. There are also json files within library.
+        location    : list
+                      Location in X,Y, and Z.
+        rotation    : list
+                      Rotation in X,Y, and Z.
         """
-        self.path_to_catalog = "{}/plano_convex_lenses.json".format(os.path.dirname(odak.catalog.__file__))
-        self.settings        = tools.load_dictionary(self.path_to_catalog)
+        self.item            = item
+        self.location        = np.asarray(location)
+        self.rotation        = np.asarray(rotation)
+        self.path_to_catalog = "{}/data/plano_convex_lenses.json".format(os.path.dirname(odak.catalog.__file__))
+        self.settings        = tools.load_dictionary(self.path_to_catalog)[self.item]
+        self.set_variables()
+        self.define_geometry()
+    
+    def set_variables(self):
+        """
+        A definition to set variables for definining plane-convex lens.
+        """
+        self.type               = self.settings["type"]
+        self.units              = self.settings["units"]
+        self.diameter           = self.settings["diameter"]
+        self.thickness          = self.settings["center thickness"]
+        self.radius             = self.settings["radius of curvature"]
+        self.coating            = self.settings["coating"]
+        self.center             = np.array([
+                                            0.,
+                                            0.,
+                                            self.radius-self.thickness 
+                                           ])
+        self.center,_,_,_       = tools.rotate_point(
+                                                     self.center,
+                                                     angles=self.rotation,
+                                                     offset=self.location
+                                                    )
+        self.plane_center       = np.array([0.,0.,0.])
 
     def define_geometry(self):
         """
         A definition to define geometry of a plano-convex lens.
         """
-        self.convex_surface = tools.define_sphere(self.center,self.radius)
-        return True
+        self.convex_surface = raytracer.define_sphere(
+                                                      self.center,
+                                                      self.radius
+                                                     )
+        self.plane_surface  = raytracer.define_circle(
+                                                      self.plane_center,
+                                                      self.diameter,
+                                                      self.rotation
+                                                     )
