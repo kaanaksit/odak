4965b502dcfb746747026345b652c055d77f2407
Kaan Akşit <kunguz@gmail.com>
2020-08-10 13:54:17 -0700

    Added a new definition odak.wave.propagate_plane_waves().

--- odak/wave/parameters.py
+++ odak/wave/parameters.py
@@ -1,5 +1,30 @@
 from odak import np
 
+def propagate_plane_waves(field,opd,k,w=0,t=0):
+    """
+    Definition to propagate a field representing a plane wave at a particular distance and time.
+
+    Parameters
+    ----------
+    field        : complex
+                   Complex field.
+    opd          : float
+                   Optical path difference in mm.
+    k            : float
+                   Wave number of a wave, see odak.wave.parameters.wavenumber for more.
+    w            : float
+                   Rotation speed of a wave, see odak.wave.parameters.rotationspeed for more.
+    t            : float
+                   Time in seconds.
+
+    Returns
+    ----------
+    new_field     : complex
+                    A complex number that provides the resultant field in the complex form A*e^(j(wt+phi)).
+    """
+    new_field = field*np.exp(1j*(-w*t+opd*k))
+    return new_field
+
 def electric_field_per_plane_wave(amplitude,opd,k,phase=0,w=0,t=0):
     """
     Definition to return state of a plane wave at a particular distance and time.
