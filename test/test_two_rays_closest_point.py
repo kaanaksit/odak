442a705b521e2c5641eef2006e26855fdbbe864e
Kaan Akşit <kunguz@gmail.com>
2020-11-10 21:53:20 -0800

    Adding a test routine for odak.raytracing.find_nearest_points() and odak.raytracing.calculate_intersection_of_two_rays() as test/test_two_rays_closest_point.py.

---
+++ test/test_two_rays_closest_point.py
@@ -0,0 +1,22 @@
+import odak.raytracing as raytracer
+import sys
+from odak import np
+
+def test():
+    start0       = np.array([0., 5.,0.])
+    start1       = np.array([0.,-5.,0.])
+    intersection = np.array([0.,0.,100.])
+    ray0         = raytracer.create_ray_from_two_points(
+                                                        start0,
+                                                        intersection
+                                                       )
+    ray1         = raytracer.create_ray_from_two_points(
+                                                        start1,
+                                                        intersection
+                                                       )
+    c0,c1        = raytracer.find_nearest_points(ray0,ray1)
+    print(c0,c1)
+    assert True==True
+
+if __name__ == '__main__':
+    sys.exit(test())
