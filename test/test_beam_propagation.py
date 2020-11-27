f730439c77addbbadd5859f486a065cfeea18002
Kaan Akşit <kunguz@gmail.com>
2020-11-27 09:30:57 -0800

    Updating bandlimited angular spectrum method.

--- test/test_beam_propagation.py
+++ test/test_beam_propagation.py
@@ -9,8 +9,8 @@ def main():
     wavelength                 = 0.5*pow(10,-6)
     pixeltom                   = 6*pow(10,-6)
     distance                   = 0.1
-    propagation_type           = 'IR Fresnel'
-#    propagation_type           = 'Bandlimited Angular Spectrum'
+#    propagation_type           = 'IR Fresnel'
+    propagation_type           = 'Bandlimited Angular Spectrum'
     k                          = wavenumber(wavelength)
     sample_field               = np.zeros((500,500),dtype=np.complex64)
     sample_field[
@@ -34,14 +34,14 @@ def main():
                                                 wavelength,
                                                 propagation_type
                                                )
-#    from odak.visualize.plotly import detectorshow
-#    detector       = detectorshow()
-#    detector.add_field(sample_field)
-#    detector.show()
-#    detector.add_field(hologram)
-#    detector.show()
-#    detector.add_field(reconstruction)
-#    detector.show()
+    from odak.visualize.plotly import detectorshow
+    detector       = detectorshow()
+    detector.add_field(sample_field)
+    detector.show()
+    detector.add_field(hologram)
+    detector.show()
+    detector.add_field(reconstruction)
+    detector.show()
     assert True==True
 
 if __name__ == '__main__':
