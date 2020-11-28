2a7c900e49b37b8bf6ecd4e6ce785c40e06f5ea1
Kaan Akşit <kunguz@gmail.com>
2020-11-27 22:53:07 -0800

    Update classical.py

--- odak/wave/classical.py
+++ odak/wave/classical.py
@@ -30,7 +30,7 @@ def propagate_beam(field,k,distance,dx,wavelength,propagation_type='IR Fresnel')
     y      = np.linspace(-nu*dx,nu*dx,nu)
     X,Y    = np.meshgrid(x,y)
     Z      = X**2+Y**2
-    if propagation_type == 'Angular Spetrum':
+    if propagation_type == 'Angular Spectrum':
        h      = 1./(1j*wavelength*distance)*np.exp(1j*k*(distance+Z/2/distance))
        h      = np.fft.fft2(np.fft.fftshift(h))*pow(dx,2)
        U1     = np.fft.fft2(np.fft.fftshift(field))
