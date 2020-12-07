765d227dbdc6ec5ef6171825baad694663c2144f
Kaan Akşit <kunguz@gmail.com>
2020-12-06 22:32:41 -0800

    :Creating a new submodule and moving files accordingly.

--- odak/learn/__init__.py
+++ odak/learn/__init__.py
@@ -1,8 +1,6 @@
 """
-``odak.learn``
+``odak.learn.wave``
 ===================
 Provides necessary definitions for neural networks and learning algorithms. The definitions are based on torch framework.
 """
 import odak.wave
-
-
