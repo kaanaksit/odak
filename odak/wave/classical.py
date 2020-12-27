ec5cc4cb13affc72775229c5ee1fca7557338698
Kaan Akşit <kunguz@gmail.com>
2020-12-27 11:30:47 +0300

    Gerchberg-Saxton 3D supports double constraint method.

--- odak/wave/classical.py
+++ odak/wave/classical.py
@@ -575,7 +575,7 @@ def point_wise(field,distances,k,dx,wavelength,lens_method='ideal',propagation_m
         hologram += lens*field[i,j]
     return hologram
 
-def gerchberg_saxton_3d(fields,n_iterations,distances,dx,wavelength,slm_range=6.28,propagation_type='IR Fresnel',initial_phase=None):
+def gerchberg_saxton_3d(fields,n_iterations,distances,dx,wavelength,slm_range=6.28,propagation_type='IR Fresnel',initial_phase=None,target_type='no constraint'):
     """
     Definition to compute a multi plane hologram using an iterative method called Gerchberg-Saxton phase retrieval algorithm. For more on the method, see: Zhou, Pengcheng, et al. "30.4: Multi‐plane holographic display with a uniform 3D Gerchberg‐Saxton algorithm." SID Symposium Digest of Technical Papers. Vol. 46. No. 1. 2015.
 
@@ -595,15 +595,18 @@ def gerchberg_saxton_3d(fields,n_iterations,distances,dx,wavelength,slm_range=6.
                        Type of the propagation (IR Fresnel, TR Fresnel, Fraunhofer).
     initial_phase    : np.complex64
                        Phase to be added to the initial value.
+    target_type      : str
+                       Target type. `No constraint` targets the input target as is. `Double constraint` follows the idea in this paper, which claims to suppress speckle: Chang, Chenliang, et al. "Speckle-suppressed phase-only holographic three-dimensional display based on double-constraint Gerchberg–Saxton algorithm." Applied optics 54.23 (2015): 6994-7001. 
 
     Result
     ---------
     hologram         : np.complex
                        Calculated complex hologram.
     """
-    k        = wavenumber(wavelength)
-    targets  = calculate_amplitude(np.asarray(fields))
-    hologram = generate_complex_field(np.zeros(targets[0].shape),0)
+    k              = wavenumber(wavelength)
+    targets        = calculate_amplitude(np.asarray(fields)).astype(np.float)
+    targets_summed = np.sum(targets,axis=0)
+    hologram       = generate_complex_field(np.zeros(targets[0].shape),0)
     if type(initial_phase) == type(None):
         hologram = add_random_phase(hologram)
     else:
@@ -612,7 +615,13 @@ def gerchberg_saxton_3d(fields,n_iterations,distances,dx,wavelength,slm_range=6.
         for distance_id in tqdm(range(len(distances))):
             distance       = distances[distance_id]
             reconstruction = propagate_beam(hologram,k,distance,dx,wavelength,propagation_type)
-            reconstruction = generate_complex_field(targets[distance_id],calculate_phase(reconstruction))
+            if target_type == 'double constraint':
+                alpha = 10.0; beta=1.;gamma=1.0
+                target_current                    = 2*alpha*np.copy(targets[distance_id])-beta*calculate_amplitude(reconstruction)
+                target_current[target_current==0] = gamma*reconstruction[target_current==0]
+            elif target_type == 'no constraint':
+                target_current = np.copy(targets[distance_id])
+            reconstruction = generate_complex_field(target_current,calculate_phase(reconstruction))
             hologram       = propagate_beam(reconstruction,k,-distance,dx,wavelength,propagation_type)
             hologram       = generate_complex_field(1.,calculate_phase(hologram)) 
     return hologram
