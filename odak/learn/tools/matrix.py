db157238123f933af189319a80a2c30ce7c0866f
Kaan Akşit <kunguz@gmail.com>
2021-01-22 16:13:25 +0000

    Odak.learn.tools.crop_center() is added.

--- odak/learn/tools/matrix.py
+++ odak/learn/tools/matrix.py
@@ -33,3 +33,30 @@ def zero_pad(field,size=None,method='center'):
     if type(size) != type(None):
         field_zero_padded = field_zero_padded[0:size[0],0:size[1]]
     return field_zero_padded
+
+def crop_center(field,size=None):
+    """
+    Definition to crop the center of a field with 2Mx2N size. The outcome is a MxN array.
+
+    Parameters
+    ----------
+    field       : ndarray
+                  Input field 2Mx2N array.
+
+    Returns
+    ----------
+    cropped     : ndarray
+                  Cropped version of the input field.
+    """
+    if type(size) == type(None):
+        qx      = int(torch.ceil(torch.tensor(field.shape[0])/4))
+        qy      = int(torch.ceil(torch.tensor(field.shape[1])/4))
+        cropped = field[qx:3*qx,qy:3*qy]
+    else:
+        cx      = int(torch.ceil(torch.tensor(field.shape[0]/2)))
+        cy      = int(torch.ceil(torch.tensor(field.shape[1]/2)))
+        hx      = int(torch.ceil(torch.tensor(size[0]/2)))
+        hy      = int(torch.ceil(torch.tensor(size[1]/2)))
+        cropped = field[cx-hx:cx+hx,cy-hy:cy+hy]
+    return cropped
+
