28025245954f31afe238036710422e528cef06f0
Kaan Akşit <kunguz@gmail.com>
2021-01-31 17:02:03 +0000

    Updating odak.learn.wave.__init__.py.

--- odak/learn/wave/__init__.py
+++ odak/learn/wave/__init__.py
@@ -112,6 +112,27 @@ def generate_complex_field(amplitude,phase):
     field     = amplitude*torch.cos(phase)+1j*amplitude*torch.sin(phase)
     return field
 
+def adjust_phase_only_slm_range(native_range,working_wavelength,native_wavelength):
+    """
+    Definition for calculating the phase range of the Spatial Light Modulator (SLM) for a given wavelength. Here you prove maximum angle as the lower bound is typically zero. If the lower bound isn't zero in angles, you can use this very same definition for calculating lower angular bound as well.
+
+    Parameters
+    ----------
+    native_range       : float
+                         Native range of the phase only SLM in radians (i.e. two pi).
+    working_wavelength : float
+                         Wavelength of the illumination source or some working wavelength.
+    native_wavelength  : float
+                         Wavelength which the SLM is designed for.
+
+    Returns
+    ==========
+    new_range          : float
+                         Calculated phase range in radians.
+    """
+    new_range = native_range*working_wavelength/native_wavelength
+    return new_range
+
 def produce_phase_only_slm_pattern(hologram,slm_range,bits=8,default_range=6.28,illumination=None):
     """
     Definition for producing a pattern for a phase only Spatial Light Modulator (SLM) using a given field.
