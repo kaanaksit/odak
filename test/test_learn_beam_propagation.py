baad72cab7e6b2dcecf11a1944ac2f41e1444c54
Kaan Akşit <kunguz@gmail.com>
2021-02-01 12:09:09 +0000

    Updating the test script for beam propagation in torch.

--- test/test_learn_beam_propagation.py
+++ test/test_learn_beam_propagation.py
@@ -4,66 +4,14 @@ import odak
 from odak import np
 from odak.wave import wavenumber,propagate_beam
 from odak.learn.wave import propagate_beam as propagate_beam_torch
+from odak.tools import zero_pad
 import torch
-
-def main():
-    # Variables to be set.
-    wavelength                 = 0.5*pow(10,-6)
-    pixeltom                   = 6*pow(10,-6)
-    distance                   = 0.2
-    propagation_type           = 'Bandlimited Angular Spectrum'
-    k                          = wavenumber(wavelength)
-    sample_field               = np.zeros((500,500),dtype=np.complex64)
-    sample_field[
-                 240:260,
-                 240:260
-                ]              = 1000
-    random_phase               = np.pi*np.random.random(sample_field.shape)
-    sample_field               = sample_field*np.cos(random_phase)+1j*sample_field*np.sin(random_phase)
-    criterion = torch.nn.MSELoss()
-
-    if np.__name__ == 'cupy':
-        sample_field = np.asnumpy(sample_field)
-
-    sample_field               = torch.from_numpy(sample_field)
-
-    hologram                   = propagate_beam_torch(
-                                                sample_field,
-                                                k,
-                                                distance,
-                                                pixeltom,
-                                                wavelength,
-                                                propagation_type
-                                               )
-    hologram.requires_grad = True
-    reconstruction             = propagate_beam_torch(
-                                                hologram,
-                                                k,
-                                                -distance,
-                                                pixeltom,
-                                                wavelength,
-                                                propagation_type
-                                               )
-    loss = criterion(torch.abs(sample_field), torch.abs(reconstruction))
-    loss.backward()
-    print(hologram.grad)
-    print('backward successfully')
-
-    #from odak.visualize.plotly import detectorshow
-    #detector       = detectorshow()
-    #detector.add_field(sample_field)
-    #detector.show()
-    #detector.add_field(hologram)
-    #detector.show()
-    #detector.add_field(reconstruction)
-    #detector.show()
-    assert True==True
 
 def compare():
     wavelength                 = 0.5*pow(10,-6)
     pixeltom                   = 6*pow(10,-6)
     distance                   = 0.2
-    propagation_type           = 'Bandlimited Angular Spectrum'
+    propagation_type           = 'TR Fresnel'
     k                          = wavenumber(wavelength)
     sample_field               = np.zeros((500,500),dtype=np.complex64)
     sample_field[
@@ -72,29 +20,30 @@ def compare():
                 ]              = 1000
     random_phase               = np.pi*np.random.random(sample_field.shape)
     sample_field               = sample_field*np.cos(random_phase)+1j*sample_field*np.sin(random_phase)
-    
+    sample_field               = zero_pad(sample_field)
+
     if np.__name__ == 'cupy':
         sample_field = np.asnumpy(sample_field)
 
     sample_field_torch         = torch.from_numpy(sample_field)
 
     ## Propagate and reconstruct using torch.
-    hologram_torch                   = propagate_beam_torch(
-                                                sample_field_torch,
-                                                k,
-                                                distance,
-                                                pixeltom,
-                                                wavelength,
-                                                propagation_type
-                                               )
-    reconstruction_torch             = propagate_beam_torch(
-                                                hologram_torch,
-                                                k,
-                                                -distance,
-                                                pixeltom,
-                                                wavelength,
-                                                propagation_type
-                                               )   
+    hologram_torch             = propagate_beam_torch(
+                                                      sample_field_torch,
+                                                      k,
+                                                      distance,
+                                                      pixeltom,
+                                                      wavelength,
+                                                      propagation_type
+                                                     )
+    reconstruction_torch       = propagate_beam_torch(
+                                                      hologram_torch,
+                                                      k,
+                                                      -distance,
+                                                      pixeltom,
+                                                      wavelength,
+                                                      propagation_type
+                                                     )   
 
     ## Propagate and reconstruct using np.
     hologram                   = propagate_beam(
@@ -113,7 +62,9 @@ def compare():
                                                 wavelength,
                                                 propagation_type
                                                )
-    np.testing.assert_array_almost_equal(hologram_torch.numpy(), hologram, 3)
+    np.testing.assert_array_almost_equal(hologram_torch.numpy(), hologram, 5)
+    np.testing.assert_array_almost_equal(reconstruction_torch.numpy(), reconstruction, 5)
+    assert True==True
 
 if __name__ == '__main__':
     sys.exit(compare())
