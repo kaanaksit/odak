de432b15bf1722948b7cd70391d88207f00016a3
Kaan <kaanaksit@kaanaksit.com>
2021-07-12 21:06:26 +0100

    Updating SGD.

--- test/test_learn_sgd.py
+++ test/test_learn_sgd.py
@@ -1,19 +1,17 @@
 import sys
 
 def test():
-    from odak import np
     import torch
     from odak.learn.wave import stochastic_gradient_descent
-    from odak.learn.tools import save_image
     wavelength               = 0.000000532
     dx                       = 0.0000064
     distance                 = 0.1
     resolution               = [1080,1920]
     cuda                     = False
     device                   = torch.device("cuda" if cuda else "cpu")
-    input_field              = torch.zeros((resolution[0],resolution[1])).to(device)
-    input_field[500::600,:] += 1
-    iteration_number         = 30
+    input_field              = torch.zeros(resolution[0],resolution[1]).to(device)
+    input_field[500::600,:]  = 1
+    iteration_number         = 50
     hologram,reconstructed = stochastic_gradient_descent(
                                                          input_field,
                                                          wavelength,
@@ -22,7 +20,7 @@ def test():
                                                          resolution,
                                                          'TR Fresnel',
                                                          iteration_number,
-                                                         learning_rate=1.0,
+                                                         learning_rate=0.1,
                                                          cuda=cuda
                                                         )
     assert True==True
