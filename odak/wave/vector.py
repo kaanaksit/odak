761cbdfdc6251cd57fff6a9130bb7827d679b12b
Kaan <kaanaksit@kaanaksit.com>
2021-09-26 09:43:40 +0100

    Initiating new documentation.

--- odak/wave/vector.py
+++ odak/wave/vector.py
@@ -3,7 +3,7 @@ from odak.tools import distance_between_two_points
 
 def propagate_field(points0,points1,field0,wave_number,direction=1):
     """
-    Definition to propagate a field from points to an another points in space.
+    Definition to propagate a field from points to an another points in space: propagate a given array of spherical sources to given set of points in space.
 
     Parameters
     ----------
@@ -89,96 +89,3 @@ def electric_field_per_plane_wave(amplitude,opd,k,phase=0,w=0,t=0):
     """
     field = amplitude*np.exp(1j*(-w*t+opd*k+phase))/opd**2
     return field
-
-def calculate_phase(field,deg=False):
-    """
-    Definition to calculate phase of a single or multiple given electric field(s).
-
-    Parameters
-    ----------
-    field        : ndarray.complex or complex
-                   Electric fields or an electric field.
-    deg          : bool
-                   If set True, the angles will be returned in degrees.
-
-    Returns
-    ----------
-    phase        : float
-                   Phase or phases of electric field(s) in radians.
-    """
-    phase = np.angle(field)
-    if deg == True:
-        phase *= 180./np.pi
-    return phase
-
-def calculate_amplitude(field):
-    """
-    Definition to calculate amplitude of a single or multiple given electric field(s).
-
-    Parameters
-    ----------
-    field        : ndarray.complex or complex
-                   Electric fields or an electric field.
-
-    Returns
-    ----------
-    amplitude    : float
-                   Amplitude or amplitudes of electric field(s).
-    """
-    amplitude = np.abs(field)
-    return amplitude
-
-def calculate_intensity(field):
-    """
-    Definition to calculate intensity of a single or multiple given electric field(s).
-
-    Parameters
-    ----------
-    field        : ndarray.complex or complex
-                   Electric fields or an electric field.
-
-    Returns
-    ----------
-    intensity    : float
-                   Intensity or intensities of electric field(s).
-    """
-    intensity = np.abs(field)**2
-    return intensity
-
-def wavenumber(wavelength):
-    """
-    Definition for calculating the wavenumber of a plane wave.
-
-    Parameters
-    ----------
-    wavelength   : float
-                   Wavelength of a wave in mm.
-
-    Returns
-    ----------
-    k            : float
-                   Wave number for a given wavelength.
-    """
-    k = 2*np.pi/wavelength
-    return k
-
-def rotationspeed(wavelength,c=3*10**11):
-    """
-    Definition for calculating rotation speed of a wave (w in A*e^(j(wt+phi))).
-
-    Parameters
-    ----------
-    wavelength   : float
-                   Wavelength of a wave in mm.
-    c            : float
-                   Speed of wave in mm/seconds. Default is the speed of light in the void!
-
-    Returns
-    ----------
-    w            : float
-
-    """
-    f = c*wavelength
-    w = 2*np.pi*f
-    return w
-
