43bc34fbea843ad941940ee67c88b15f7a65ffae
zhurz21 <author@corpmail.com>
2021-12-07 13:43:46 +0800

    add some keyword parameter to test file

--- test/test_learn_loss.py
+++ test/test_learn_loss.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 import sys
 from odak.learn.wave import generate_complex_field, phase_gradient, speckle_contrast
 
@@ -9,10 +10,20 @@ def main():
     amplitude = torch.rand_like(phase)
     complex_field = generate_complex_field(amplitude, phase)
     kernel = torch.tensor([[[[0, -1, 0], [-1, 4, -1], [0, -1, 0]]]], dtype=torch.float32)/4
-    kernel_size = 5
+    kernel_size = 7
+    step_size = (3, 3)
 
-    phase_gradient_calculator = phase_gradient(kernel = kernel)
-    speckle_contrast_calculator = speckle_contrast(kernel_size = kernel_size)
+    phase_gradient_calculator = phase_gradient(
+        kernel = kernel,
+        loss = nn.MSELoss()
+    )
+
+    speckle_contrast_calculator = speckle_contrast(
+        kernel_size = kernel_size,
+        step_size = step_size,
+        loss = nn.MSELoss()
+    )
+    
     phase_gradient_regular = phase_gradient_calculator(torch.angle(complex_field))
     speckle_contrast_regular = speckle_contrast_calculator(torch.abs(complex_field))
     print('The phase gradient regularization is: {:.3f}'.format(phase_gradient_regular))
