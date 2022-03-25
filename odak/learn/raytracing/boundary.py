6dafff733a85b5754974534160ca5d840ca2846c
Kaan <kaanaksit@kaanaksit.com>
2022-03-25 00:43:14 +0000

    Adding odak.learn.refract().

--- odak/learn/raytracing/boundary.py
+++ odak/learn/raytracing/boundary.py
@@ -2,20 +2,68 @@ import torch
 from odak.learn.raytracing.primitives import is_it_on_triangle, center_of_triangle
 
 
+def refract(vector, normvector, n1, n2, error=0.01):
+    """
+    Definition to refract an incoming ray.
+
+    Parameters
+    ----------
+    vector         : torch.tensor
+                     Incoming ray.
+    normvector     : torch.tensor
+                     Normal vector.
+    n1             : float
+                     Refractive index of the incoming medium.
+    n2             : float
+                     Refractive index of the outgoing medium.
+    error          : float 
+                     Desired error.
+
+    Returns
+    -------
+    VectorOutput   : torch.tensor
+                     Refracted ray.
+    """
+    mu    = n1 / n2
+    div   = normvector[1,0]**2  + normvector[1,1]**2 + normvector[1,2]**2
+    a     = mu * (vector[1,0] * normvector[1,0] + vector[1,1] * normvector[1,1] + vector[1,2] * normvector[1,2]) / div
+    b     = (mu**2 - 1) / div
+    to    = -b * 0.5 / a
+    num   = 0
+    eps   = error * 2
+    while eps > error:
+       num   += 1
+       oldto  = to
+       v      = to**2 + 2 * a * to + b
+       deltav = 2 * (to + a)
+       to     = to - v / deltav
+       eps    = abs(oldto - to)
+    if num > 5000:
+       return False
+    VectorOutput = torch.zeros_like(vector)
+    VectorOutput[0,0] = normvector[0,0]
+    VectorOutput[0,1] = normvector[0,1]
+    VectorOutput[0,2] = normvector[0,2]
+    VectorOutput[1,0] = mu * vector[1,0] + to * normvector[1,0]
+    VectorOutput[1,1] = mu * vector[1,1] + to * normvector[1,1]
+    VectorOutput[1,2] = mu * vector[1,2] + to * normvector[1,2]
+    return VectorOutput
+
+
 def reflect(input_ray, normal):
     """ 
     Definition to reflect an incoming ray from a surface defined by a surface normal. Used method described in G.H. Spencer and M.V.R.K. Murty, "General Ray-Tracing Procedure", 1961.
 
     Parameters
     ----------
-    input_ray    : ndarray
+    input_ray    : torch.tensor
                    A vector/ray (2x3). It can also be a list of rays (nx2x3).
-    normal       : ndarray
+    normal       : torch.tensor
                    A surface normal (2x3). It also be a list of normals (nx2x3).
 
     Returns
     ----------
-    output_ray   : ndarray
+    output_ray   : torch.tensor
                    Array that contains starting points and cosines of a reflected ray.
     """
     input_ray = torch.tensor(input_ray)
@@ -58,7 +106,7 @@ def intersect_w_triangle(ray, triangle):
     """
     normal, distance = intersect_w_surface(ray, triangle)
     if is_it_on_triangle(normal[0], triangle[0], triangle[1], triangle[2]) == False:
-        return 0, 0
+        return None, None
     return normal, distance
 
 
@@ -87,11 +135,11 @@ def intersect_w_surface(ray, points):
         points = points.reshape((1, 3, 3))
     if len(normal.shape) == 2:
         normal = normal.reshape((1, 2, 3))
-    f = normal[:, 0]-ray[:, 0]
-    distance = torch.mm(normal[:, 1], f.T)/torch.mm(normal[:, 1], ray[:, 1].T)
+    f = normal[:, 0] - ray[:, 0]
+    distance = torch.mm(normal[:, 1], f.T) / torch.mm(normal[:, 1], ray[:, 1].T)
     n = int((torch.max(torch.tensor([ray.shape[0], normal.shape[0]]))))
-    normal = torch.zeros((n, 2, 3))
-    normal[:, 0] = ray[:, 0] + distance.T*ray[:, 1]
+    #normal = torch.zeros((n, 2, 3))
+    normal[:, 0] = ray[:, 0] + distance.T * ray[:, 1]
     distance = torch.abs(distance)
     if normal.shape[0] == 1:
         normal = normal.reshape((2, 3))
@@ -121,12 +169,14 @@ def get_triangle_normal(triangle, triangle_center=None):
         triangle = triangle.reshape((1, 3, 3))
     normal = torch.zeros((triangle.shape[0], 2, 3)).to(triangle.device)
     direction = torch.cross(
-        triangle[:, 0]-triangle[:, 1], triangle[:, 2]-triangle[:, 1])
+                            triangle[:, 0] - triangle[:, 1], 
+                            triangle[:, 2] - triangle[:, 1]
+                           )
     if type(triangle_center) == type(None):
         normal[:, 0] = center_of_triangle(triangle)
     else:
         normal[:, 0] = triangle_center
-    normal[:, 1] = direction/torch.sum(direction, axis=1)[0]
+    normal[:, 1] = direction / torch.sum(direction, axis=1)[0]
     if normal.shape[0] == 1:
         normal = normal.reshape((2, 3))
     return normal
