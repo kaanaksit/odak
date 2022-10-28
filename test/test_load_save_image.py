ac96c715e61d154c4bda58a0323172e15c518c6a
Koray Kavakli <35215843+KorayKavakli@users.noreply.github.com>
2022-10-28 23:07:10 +0300

    Update test_load_save_image.py

--- test/test_load_save_image.py
+++ test/test_load_save_image.py
@@ -2,7 +2,7 @@ import sys
 from odak.tools import load_image, save_image
 
 def test():
-    image = load_image("sample.png")
+    image = load_image("./test/sample.png")
     save_image("sample_out.png", image, bit=16, cmin=0., cmax=65535.)
     assert True == True
 
