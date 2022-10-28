85b3495b4799c2f93bd36cb78dcec7c6c3a9dcd8
Koray Kavakli <35215843+KorayKavakli@users.noreply.github.com>
2022-10-28 22:58:47 +0300

    Update test_learn_load_save_image.py

--- test/test_learn_load_save_image.py
+++ test/test_learn_load_save_image.py
@@ -3,7 +3,7 @@ from odak.learn.tools import load_image, save_image
 
 def test():
     image = load_image("sample.png")
-    save_image("sample_out.png", image, bit=16, cmin=0., cmax=65535.)
+    save_image("./test/sample_out.png", image, bit=16, cmin=0., cmax=65535.)
     assert True == True
 
 if __name__ == '__main__':
