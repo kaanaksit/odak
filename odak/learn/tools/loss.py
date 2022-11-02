593796558967e9447305f5aafe5342549cdde7a9
Kaan <kaanaksit@kaanaksit.com>
2022-11-02 08:49:07 +0000

    odak.learn.tools.total_variation_loss() is added.

--- odak/learn/tools/loss.py
+++ odak/learn/tools/loss.py
@@ -22,3 +22,28 @@ def psnr(image, ground_truth, peak_value=1.0):
     mse = torch.mean((ground_truth - image)**2)
     result = 20 * torch.log10(peak_value / torch.sqrt(mse))
     return result
+
+
+def total_variation_loss(frame):
+    """
+    Function for evaluating a frame against a target using total variation approach.
+        
+    Parameters
+    ----------
+    frame         : torch.tensor
+                    Input frame [1 x 3 x m x n] or [3 x m x n] or [m x n].
+
+    Returns
+    -------
+    loss          : float
+                    Loss from evaluation.
+    """
+    if len(frame.shape) == 2:
+        frame = frame.unsqueeze(0)
+    if len(frame.shape) == 3:
+        frame = frame.unsqueeze(0)
+    diff_x = frame[:, :, :, 1:] - frame[:, :, :, :-1]
+    diff_y = frame[:, :, 1:, :] - frame[:, :, :-1, :]
+    pixel_count = frame.shape[0] * frame.shape[1] * frame.shape[2] * frame.shape[3]
+    loss = ((diff_x**2).sum() + (diff_y**2).sum()) / pixel_count
+    return loss
