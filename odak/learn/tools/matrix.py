76d2b5c08425098f3c12ef40090956e8083f048f
Kaan <kaanaksit@kaanaksit.com>
2022-11-30 14:02:54 +0000

    Adding odak.learn.tools.convert_rgb_to_linear_rgb().

--- odak/learn/tools/matrix.py
+++ odak/learn/tools/matrix.py
@@ -191,15 +191,38 @@ def convert_rgb_to_yuv(image):
     Returns
     -------
     image_yuv       : torch.tensor
-                      Output image in YUV color space [k x 3 x m x n].
+                      Output image in YUV color space [k x 3 x m x n] or [1 x 3 x m x n].
     """
     if len(image.shape) == 3:
        image = image.unsqueeze(0)
     red = image[:, 0, :, :]
     green = image[:, 1, :, :]
     blue  = image[:, 2, :, :]
-    y = 0.299 * red + 0.587 * green + 0.114 * blue
-    u = -0.147 * red - 0.289 * green + 0.436 * blue
-    v = 0.615 * red - 0.515 * green - 0.100 * blue 
-    image_yuv = torch.stack([y, u, v], dim=0)
+    image_yuv = torch.zeros_like(image)
+    image_yuv[:, 0, :, :] = 0.299 * red + 0.587 * green + 0.114 * blue
+    image_yuv[:, 1, :, :] = -0.147 * red - 0.289 * green + 0.436 * blue
+    image_yuv[:, 2, :, :] = 0.615 * red - 0.515 * green - 0.100 * blue 
     return image_yuv
+
+
+def convert_rgb_to_linear_rgb(image, threshold = 0.0031308):
+    """
+    Definition to convert RGB images to linear RGB color space.
+
+    Parameters
+    ----------
+    image           : torch.tensor
+                      Input image in RGB color space [k x 3 x m x n] or [3 x m x n]. Image(s) must be normalized between zero and one.
+    threshold       : float
+                      Threshold used in calculations.
+
+    Returns
+    -------
+    image_linear    : torch.tensor
+                      Output image in linear RGB color space [k x 3 x m x n] or [1 x 3 x m x n].
+    """
+    if len(image.shape) == 3:
+        image = image.unsqueeze(0)
+    threshold = 0.0031308
+    image_linear =  torch.where(image > threshold, 1.055 * torch.pow(image.clamp(min=threshold), 1 / 2.4) - 0.055, 12.92 * image)
+    return image_linear
