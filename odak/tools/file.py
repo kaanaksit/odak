7ea4179281c13f80956fdffb9e11148ab753121a
Kaan <kaanaksit@kaanaksit.com>
2023-03-05 15:33:20 +0000

    Fixing a bug related to odak.tools.save_dictionary(), supporting /home/kaan directory.

--- odak/tools/file.py
+++ odak/tools/file.py
@@ -173,7 +173,7 @@ def save_dictionary(settings, filename):
     filename      : str
                     Filename.
     """
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(os.path.expanduser(filename), 'w', encoding='utf-8') as f:
         json.dump(settings, f, ensure_ascii=False, indent=4)
     return settings
 
