6dec26d14ea811f088bd6605171b8181be0383cd
Kaan Akşit <kunguz@gmail.com>
2023-07-03 14:21:19 +0100

    Updates in detector class.

--- odak/learn/raytracing/detector.py
+++ odak/learn/raytracing/detector.py
@@ -60,8 +60,7 @@ class detector():
                                                    )
         self.pixel_locations = self.pixel_locations.to(self.device)
         self.clear()
-        self.sigmoid = torch.nn.Sigmoid()
-        self.threshold = torch.nn.Threshold(0.8, 0)
+        self.threshold = torch.nn.Threshold(- self.pixel_diagonal_half_size, -1e10)
 
 
     def intersect(self, rays, color = 0):
@@ -79,26 +78,74 @@ class detector():
 
         Returns
         -------
-        image           : torch.tensor
-                          Image on the image sensor [3 x k x l].
+        points          : torch.tensor
+                          Intersection points with the image detector [k x 3].
         """
         normals, _ = intersect_w_surface(rays, self.plane)
         points = normals[:, 0]
-        #print(self.pixel_locations.shape);import sys;sys.exit()
         distances = torch.sqrt(torch.sum((points.unsqueeze(1) - self.pixel_locations.unsqueeze(0)) ** 2, dim = 2))
-        #hit = distances * (distances < self.pixel_diagonal_half_size)        
-        #hit = torch.nan_to_num(hit / hit, nan = 0.)
-        hit = torch.abs(1. - 1. / (1. + torch.exp(- 50. * distances + 5)))
+        close = - self.threshold(- distances)
+        hit = self.sigmoid(close)
         image = torch.sum(hit, dim = 0)
-        self.image[color] += image.reshape(self.image.shape[-2], self.image.shape[-1])
-        return self.image, points
+        self.image[color] += image.reshape(
+                                           self.image.shape[-2], 
+                                           self.image.shape[-1]
+                                          )
+        return points
 
 
-    def convert_image_to_points(self, image):
-        image = image.reshape(image.shape[0], -1)
-        print(image.shape, self.pixel_locations.shape)
-        image_points = self.pixel_locations[image[0] > 0., :]
-        return image_points
+    def get_image(self):
+        """
+        Function to return the detector image.
+
+        Returns
+        -------
+        image           : torch.tensor
+                          Detector image.
+        """
+        image = torch.zeros_like(self.image)
+        image = (self.image - self.image.min()) / (self.image.max() - self.image.min())
+        return image
+
+
+    def sigmoid(self, value):
+        """
+        A slightly modified sigmoid function.
+
+        Parameters
+        ----------
+        value           : torch.tensor
+                          Value.
+
+        Returns
+        -------
+        result          : torch.tensor
+                          Result.
+        """
+        result = 1. - 1. / (1. + torch.exp(5. - value))
+        return result
+
+
+    def convert_image_to_points(self, image, color = 0):
+        """
+        Extracting the locations of non-zero pixels.
+
+        Parameters
+        ----------
+        image           : torch.tensor
+                          Image on a detector [k x m x n].
+        color           : int
+                          Color channel for identifying non-zero pixels.
+
+        Returns
+        -------
+        image_points    : torch.tensor
+                          Non-zero pixel locations [j x 3].
+        """
+        image = image[color].reshape(-1)
+        image_points = self.pixel_locations[image > 0., :]
+        image_values = image[image > 0.]
+        return image_points, image_values
 
 
     def clear(self):
--- test/test_learn_ray_detector.py
+++ test/test_learn_ray_detector.py
@@ -19,46 +19,45 @@ def test():
                                              )
     mesh = odak.learn.raytracing.planar_mesh(
                                              size = [10., 10.],
-                                             number_of_meshes = [10, 10],
+                                             number_of_meshes = [20, 20],
                                              offset = torch.tensor([0., 0., 10.]),
                                              device = device
                                             )
     start_points, _, _, _ = odak.learn.tools.grid_sample(
-                                                         no = [10, 10],
+                                                         no = [70, 70],
                                                          size = [9., 9.],
                                                          center = [0., 0., 0.]
                                                         )
     end_points, _, _, _ = odak.learn.tools.grid_sample(
-                                                       no = [10, 10],
+                                                       no = [70, 70],
                                                        size = [9., 9.],
                                                        center = [0., 0., 10.]
                                                       )
     start_points = start_points.to(device)
     end_points = end_points.to(device)
     rays = odak.learn.raytracing.create_ray_from_two_points(start_points, end_points)
-    target = torch.zeros(1, 100, 100, device = device)
-    target[0, 50, 20] = 1.
-    target[0, 50, 80] = 1.
-    target[0, 50, 50] = 1.
-    target[0, 80, 50] = 1.
-    target[0, 20, 50] = 1.
+    assert True == True
+    #target = torch.zeros(1, 100, 100, device = device)
+    #target[0, 45:55, 45:55:] = torch.linspace(0., 1., 10)
+    target = odak.learn.tools.load_image('test/k.png', normalizeby = 255., torch_style = True)[1].unsqueeze(0).to(device)
     learning_rate = 2e-2
-    number_of_steps = 1
+    number_of_steps = 1000
     optimizer = torch.optim.AdamW([mesh.heights], lr = learning_rate)
-    loss_function = torch.nn.MSELoss(reduction = 'sum')
     t = tqdm(range(number_of_steps), leave = False, dynamic_ncols = True)
+    target_points, target_values = detector.convert_image_to_points(target)
+    loss_function = torch.nn.MSELoss()
     odak.learn.tools.save_image('target.png', target, cmin = 0., cmax = 1.)
-    target_points = detector.convert_image_to_points(target)
     for step in t:
         optimizer.zero_grad()
         detector.clear()
         reflected_rays, _ = mesh.mirror(rays)
-        image, points = detector.intersect(reflected_rays)
-        loss = torch.sqrt(torch.sum((points.unsqueeze(1) - target_points.unsqueeze(0)) ** 2, dim = 2))
-        loss = torch.min(loss, dim = 1).values
-#        print(loss.shape);import sys;sys.exit()
-        loss = torch.sum(loss)
-#        loss = loss_function(points, torch.zeros(1, 3, device = device))
+        points = detector.intersect(reflected_rays)
+        image = detector.get_image()
+        distances = torch.sqrt(torch.sum((points.unsqueeze(1) - target_points.unsqueeze(0)) ** 2, dim = 2))
+        loss_location = torch.sum(torch.min(distances, dim = 1).values)
+        #loss_image = loss_function(image, target)
+        #loss = loss_image #+ loss_location
+        loss = loss_location
         loss.backward(retain_graph = True)
         optimizer.step()
         description = 'Loss: {}'.format(loss.item())
