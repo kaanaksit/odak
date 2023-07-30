2a17d0adb73a4ab36c9c2cfe39359fd1707c3e6d
aguzel <ahmethamdiguzel@gmail.com>
2023-07-30 23:20:27 +0100

    color mapping and test added

---
+++ test/test_color_map.py
@@ -0,0 +1,14 @@
+import odak
+import torch
+import sys
+
+def test():
+    device = torch.device('cpu')
+    input_srgb_image = torch.randn((3, 256, 256)).to(device)
+    target_srgb_image = torch.randn((3, 256, 256)).to(device)
+    mapped_srgb_image = odak.learn.perception.color_map.color_map(input_srgb_image, target_srgb_image, model = 'Lab Stats')
+
+    assert True == True
+
+if __name__ == "__main__":
+    sys.exit(test())
