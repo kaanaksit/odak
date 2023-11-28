9142f7cd2ef0c2ccfa59a688f422447064c5b44c
kaanaksit <kunguz@gmail.com>
2023-11-28 21:53:36 +0000

    Unit test for odak.learn.models.multi_layer_perceptron() is updated.

--- test/test_learn_models_multi_layer_perceptron.py
+++ test/test_learn_models_multi_layer_perceptron.py
@@ -10,16 +10,16 @@ def main():
     filename = './test/fruit_lady.png'
     test_filename  = './estimation.png'
     weights_filename = 'model_weights.pt'
-    learning_rate = 1e-3
-    no_epochs = 1000000
+    learning_rate = 1e-2
+    no_epochs = 50
     number_of_batches = 1
-    dimensions = [2, 80, 40, 20, 3]
-    device_name = 'cuda'
-    save_at_every = 2000
+    dimensions = [2, 220, 100, 3]
+    device_name = 'cpu'
+    save_at_every = 50
     device = torch.device(device_name)
     model = odak.learn.models.multi_layer_perceptron(
                                                      dimensions = dimensions,
-    #                                                 activation = torch.nn.Tanh(),
+                                                     activation = torch.nn.Tanh(),
                                                      bias = True,
                                                      periodic = True
                                                     ).to(device)
@@ -39,19 +39,20 @@ def main():
             description = 'train loss: {:.5f}, test loss:{:.5f}'.format(train_loss, test_loss)
             epochs.set_description(description)
             if epoch_id % save_at_every == 0: 
-                torch.no_grad()
                 odak.learn.tools.save_image(test_filename, estimation, cmin = 0., cmax = 1.)
         torch.save(model.state_dict(), weights_filename)
         print('Model weights save: {}'.format(weights_filename))
+        odak.learn.tools.save_image(test_filename, estimation, cmin = 0., cmax = 1.)
     except:
         torch.save(model.state_dict(), weights_filename)
         print('Model weights save: {}'.format(weights_filename))
+        odak.learn.tools.save_image(test_filename, estimation, cmin = 0., cmax = 1.)
     assert True == True
 
 
 def get_batches(image, number_of_batches = 100):
-    xs = torch.arange(image.shape[0] * 2)
-    ys = torch.arange(image.shape[1] * 2)
+    xs = torch.arange(image.shape[0])
+    ys = torch.arange(image.shape[1])
     XS, YS = torch.meshgrid(xs, ys, indexing = 'ij')
     XS = XS.reshape(number_of_batches, -1, 1)
     YS = YS.reshape(number_of_batches, -1, 1)
@@ -63,6 +64,9 @@ def train(output_values, input_values, optimizer, loss_function, model):
     total_loss = 0.
     for input_value in input_values:
         optimizer.zero_grad()
+        normalized_input_value = torch.zeros_like(input_value)
+        normalized_input_value[:, 0] = input_value[:, 0] / output_values.shape[0]
+        normalized_input_value[:, 1] = input_value[:, 1] / output_values.shape[1]
         estimation = model(input_value)
         ground_truth = output_values[input_value[:, 0].int(), input_value[:, 1].int(), :]
         loss = loss_function(estimation, ground_truth)
@@ -76,6 +80,9 @@ def test(output_values, input_values, loss_function, model):
     estimated_image = torch.zeros_like(output_values)
     for input_value in input_values:
         torch.no_grad()
+        normalized_input_value = torch.zeros_like(input_value)
+        normalized_input_value[:, 0] = input_value[:, 0] / output_values.shape[0]
+        normalized_input_value[:, 1] = input_value[:, 1] / output_values.shape[1]
         estimation = model(input_value)
         ground_truth = output_values[input_value[:, 0].int(), input_value[:, 1].int(), :]
         estimated_image[input_value[:, 0].int(), input_value[:, 1].int(), :] = estimation
