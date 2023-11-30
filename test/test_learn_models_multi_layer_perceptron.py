e47c5eb695f9fb13cbd5dbb901d66207a9fab01d
Kaan Akşit <kunguz@gmail.com>
2023-11-30 17:17:13 +0000

    Support for FILM SIREN in odak.learn.models.multi_layer_perceptron.

--- test/test_learn_models_multi_layer_perceptron.py
+++ test/test_learn_models_multi_layer_perceptron.py
@@ -11,10 +11,10 @@ def main():
     test_filename  = './estimation.png'
     weights_filename = 'model_weights.pt'
     learning_rate = 1e-3
-    no_epochs = 2000
+    no_epochs = 20
     number_of_batches = 1
     dimensions = [2, 128, 128, 3]
-    device_name = 'cuda'
+    device_name = 'cpu'
     save_at_every = 500
     device = torch.device(device_name)
     model = odak.learn.models.multi_layer_perceptron(
