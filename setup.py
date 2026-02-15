22ffb02e310739209244d5950f7ef755467809ec
Kaan Akşit <kunguz@gmail.com>
2026-02-15 13:41:21 +0000

    Modernizing  using an agent.

--- setup.py
+++ setup.py
@@ -1,63 +1,48 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
-
+read = lambda fname: open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 thelibFolder = os.path.dirname(os.path.realpath(__file__))
-requirementPath = thelibFolder + '/requirements.txt'
+requirementPath = os.path.join(thelibFolder, "requirements.txt")
 install_requires = []
 if os.path.isfile(requirementPath):
     with open(requirementPath) as f:
         install_requires = f.read().splitlines()
 
 setup(
-      name = "odak",
-      version = "0.2.7",
-      author = "Kaan Akşit",
-      author_email = "kaanaksit@kaanaksit.com",
-      description = "Odak, the fundamental Python library for scientific computing in optical sciences.",
-      license = read('LICENSE.txt'),
-      keywords = "optics, holography, perception, graphics",
-      url = "https://github.com/kaanaksit/odak",
-      install_requires = install_requires,
-      packages = [
-                  'odak',
-                  'odak/log',
-                  'odak/raytracing',
-                  'odak/fit',
-                  'odak/jones',
-                  'odak/tools',
-                  'odak/wave',
-                  'odak/learn/wave',
-                  'odak/learn/models',
-                  'odak/learn/tools',
-                  'odak/learn/perception',
-                  'odak/learn/raytracing',
-                  'odak/learn/lensless',
-                  'odak/visualize',
-                  'odak/visualize/plotly',
-                  'odak/visualize/blender',
-                  'odak/manager',
-                  'odak/measurement',
-                  'odak/catalog',
-                  'odak/learn'
-                 ],
-    package_dir = {'odak': 'odak'},
-    package_data = {'odak': ['catalog/data/*.json']},
-    data_files = [
-                  ('', ['LICENSE.txt', 'README.md', 'THANKS.txt', 'requirements.txt', 'short_readme.md'])
-                 ],
-    long_description = read('short_readme.md'),
-    classifiers = [
-                   'Development Status :: 5 - Production/Stable',
-                   'Intended Audience :: Science/Research',
-                   'Intended Audience :: Developers',
-                   'Topic :: Scientific/Engineering :: Physics',
-                   'Programming Language :: Python',
-                   'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
-                  ],
-    python_requires = '>=3.7.5',
+    name="odak",
+    version="0.2.7",
+    author="Kaan Akşit",
+    author_email="kaanaksit@kaanaksit.com",
+    description="Odak, the fundamental Python library for scientific computing in optical sciences.",
+    license=read("LICENSE.txt"),
+    keywords="optics, holography, perception, graphics",
+    url="https://github.com/kaanaksit/odak",
+    install_requires=install_requires,
+    packages=find_packages(where="."),
+    package_dir={"odak": "odak"},
+    package_data={"odak": ["catalog/data/*.json", "learn/models/*.json"]},
+    data_files=[
+        (
+            "",
+            [
+                "LICENSE.txt",
+                "README.md",
+                "THANKS.txt",
+                "requirements.txt",
+                "short_readme.md",
+            ],
+        )
+    ],
+    long_description=read("short_readme.md"),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MPL-2.0",
+    ],
+    python_requires=">=3.9",
 )
