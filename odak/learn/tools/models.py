e18b1fb8d61ca67660c0c652baf417684b92d198
Kaan Akşit <kunguz@gmail.com>
2026-02-19 12:04:05 +0000

    Improved docstrings for freeze and unfreeze functions in models.py

--- odak/learn/tools/models.py
+++ odak/learn/tools/models.py
@@ -3,13 +3,20 @@ import torch
 
 def freeze(model):
     """
-    A utility function to freeze the parameters of a provided model defined as a Pythonic class.
-    For instance, `odak.learn.models.unet` is such a model.
+    A utility function to freeze the parameters of a provided model.
+    
+    This function sets `requires_grad` to `False` for all parameters in the model,
+    effectively freezing them during training.
 
     Parameters
     ----------
-    model          : torch.nn.modules
-                     Model to be frozen, in other terms `requires_grad` to be set to `False`.
+    model : torch.nn.Module
+        Model whose parameters are to be frozen. This should be a PyTorch model instance.
+
+    Returns
+    -------
+    None
+        The function modifies the model in-place.
     """
     for parameter in model.parameters():
         parameter.requires_grad = False
@@ -17,14 +24,20 @@ def freeze(model):
 
 def unfreeze(model):
     """
-    A utility function to unfreeze the parameters of a provided model defined as a Pythonic class.
-    For instance, `odak.learn.models.unet` is such a model.
-
+    A utility function to unfreeze the parameters of a provided model.
+    
+    This function sets `requires_grad` to `True` for all parameters in the model,
+    effectively allowing them to be updated during training.
 
     Parameters
     ----------
-    model          : torch.nn.modules
-                     Model to unfreeze, in other terms `requires_grad` to be set to `True`.
+    model : torch.nn.Module
+        Model whose parameters are to be unfrozen. This should be a PyTorch model instance.
+
+    Returns
+    -------
+    None
+        The function modifies the model in-place.
     """
     for parameter in model.parameters():
         parameter.requires_grad = True
